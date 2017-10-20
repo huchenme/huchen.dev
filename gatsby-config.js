e3afceb050f0f89ee7f8aa0f565db02d8c981dae
Hu Chen <chen@huchen.me>
2017-10-21 00:10:37 +0800

    add GA config

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-44913635-1`,
       },
     },
     `gatsby-plugin-offline`,
