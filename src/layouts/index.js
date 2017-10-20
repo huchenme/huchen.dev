600fb5cf57f34cc2391e36bb589304e40f305b7b
Hu Chen <chen@huchen.me>
2017-10-21 00:11:36 +0800

    use title from metadata

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,71 +4,60 @@ import { Container } from 'react-responsive-grid'
 
 import { rhythm, scale } from '../utils/typography'
 
-class Template extends React.Component {
-  render() {
-    const { location, children } = this.props
-    let header
+const Template = ({ children, data, location }) => {
+  let header
+  const siteTitle = data.site.siteMetadata.title
 
-    let rootPath = `/`
-    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
-      rootPath = __PATH_PREFIX__ + `/`
-    }
+  let rootPath = `/`
+  if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
+    rootPath = __PATH_PREFIX__ + `/`
+  }
 
-    if (location.pathname === rootPath) {
-      header = (
-        <h1
-          style={{
-            ...scale(1.5),
-            marginBottom: rhythm(1.5),
-            marginTop: 0,
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to={'/'}
-          >
-            Gatsby Starter Blog
-          </Link>
-        </h1>
-      )
-    } else {
-      header = (
-        <h3
-          style={{
-            fontFamily: 'Montserrat, sans-serif',
-            marginTop: 0,
-            marginBottom: rhythm(-1),
-          }}
-        >
-          <Link
-            style={{
-              boxShadow: 'none',
-              textDecoration: 'none',
-              color: 'inherit',
-            }}
-            to={'/'}
-          >
-            Gatsby Starter Blog
-          </Link>
-        </h3>
-      )
-    }
-    return (
-      <Container
+  if (location.pathname === rootPath) {
+    header = (
+      <h1
         style={{
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          ...scale(1.5),
+          marginBottom: rhythm(1.5),
+          marginTop: 0,
         }}
       >
-        {header}
-        {children()}
-      </Container>
+        {siteTitle}
+      </h1>
+    )
+  } else {
+    header = (
+      <h3
+        style={{
+          fontFamily: 'Montserrat, sans-serif',
+          marginTop: 0,
+          marginBottom: rhythm(-1),
+        }}
+      >
+        <Link
+          style={{
+            boxShadow: 'none',
+            textDecoration: 'none',
+            color: 'inherit',
+          }}
+          to={'/'}
+        >
+          {siteTitle}
+        </Link>
+      </h3>
     )
   }
+  return (
+    <Container
+      style={{
+        maxWidth: rhythm(24),
+        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+      }}
+    >
+      {header}
+      {children()}
+    </Container>
+  )
 }
 
 Template.propTypes = {
@@ -78,3 +67,13 @@ Template.propTypes = {
 }
 
 export default Template
+
+export const query = graphql`
+  query LayoutQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
